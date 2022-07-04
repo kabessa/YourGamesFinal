6267d3ee2495c06112ecec1c96c111e59578a26a
Marcus <markbons159@gmail.com>
2022-07-04 17:33:58 -0300

    Adicionado Listagem com bd+ delete

---
+++ app/src/main/java/com/example/yourgames/DeleteGame.java
@@ -0,0 +1,14 @@
+package com.example.yourgames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_game);
+    }
+}
